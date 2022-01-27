8c517846d4bff9046427bb3e2f181f1540428df2
Michael <93211335+waveyboym@users.noreply.github.com>
2022-01-27 13:06:54 +0200

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "stack.h"
-
-int main(){
-    stack * NewStack = new stack("rotorr");
-    NewStack->traverseStackRecursively(NewStack->getTop());
-    NewStack->checkIfPalindrome();
-    delete NewStack;
-    return 0;
-}
